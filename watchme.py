f60661de59df8b1a6e35f817afec058e068385b5
Kyle Ladd <kladd6@gmail.com>
2014-10-23 16:12:36 -0400

    Added Try Catch blocks for functions.  Errors would arise when there would be a network outage or the destination directory is not writable.  If the destination does not exist, it will try to create it.

--- watchme.py
+++ watchme.py
@@ -9,47 +9,64 @@ from watchdog.events import FileSystemEventHandler
 
 
 def read_config_file(filename):
-    BASE_DIR = os.path.realpath(os.path.dirname(sys.argv[0]))
+    try:
+        BASE_DIR = os.path.realpath(os.path.dirname(sys.argv[0]))
 
-    with open(os.path.join(BASE_DIR, filename)) as data_file:
-        data = json.load(data_file)
+        with open(os.path.join(BASE_DIR, filename)) as data_file:
+            data = json.load(data_file)
 
-    return data
+        return data
+    except Exception as ex:
+        print type(ex)     # the exception instance
+        print ex
+        print "Error in reading the config file"
+        return "[]"
 
 
 def copy_resource_to_server(event):
-    """Copy source file to destination"""
-    data = read_config_file("config.json")
-
-    src_file = os.path.join(data["src"]["dir"], data["src"]["file"])
-    dest_file = os.path.join(data["dest"]["dir"], data["dest"]["file"])
-
-    if event.src_path == src_file:
-        shutil.copyfile(src_file, dest_file)
-
+    try:
+        data = read_config_file("config.json")
+        src_file = os.path.join(data["src"]["dir"], data["src"]["file"])
+        dest_file = os.path.join(data["dest"]["dir"], data["dest"]["file"])
+        #If destination directory does not exist, create it
+        if not os.path.exists(data["dest"]["dir"]):
+            os.makedirs(data["dest"]["dir"])
+        if event.src_path == src_file:
+            """Copy source file to destination"""
+            shutil.copyfile(src_file, dest_file)
+    except Exception as ex:
+        print type(ex)     # the exception instance
+        print ex
+        print "Error in copying resource to server"
 
 def _main():
-    event_handler = FileSystemEventHandler()
-
-    event_handler.on_created = copy_resource_to_server
-    event_handler.on_modified = copy_resource_to_server
-
-    observer = Observer()
-
-    data = read_config_file("config.json")
-    observer.schedule(event_handler, data["src"]["dir"], recursive=False)
-
-    observer.start()
-
     try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        observer.stop()
+        event_handler = FileSystemEventHandler()
 
-    observer.join()
+        event_handler.on_created = copy_resource_to_server
+        event_handler.on_modified = copy_resource_to_server
 
-    raw_input("")
+        observer = Observer()
+
+        data = read_config_file("config.json")
+        observer.schedule(event_handler, data["src"]["dir"], recursive=False)
+
+        observer.start()
+
+        try:
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            observer.stop()
+
+        observer.join()
+
+        raw_input("")
+    except Exception as ex:
+        print type(ex)     # the exception instance
+        print ex
+        print "Error in main"
+
 
 
 if __name__ == "__main__":
